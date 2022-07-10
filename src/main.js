c4d78c36d8d6f7f01c9306a86c130e75fde8abed
Radgus <dominguezradgus@gmail.com>
2022-07-10 15:16:33 -0500

    api key movies added

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-console.log(API_KEY);
+import axios from "axios";
+
+
+const API = axios.create({
+  baseURL: 'https://api.thecatapi.com/v1',
+})
+
+
+const getTrendingMovies = async () => {
+  const res=await fetch
+}
